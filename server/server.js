f0f075b6b678c62cd436dc191c7fb860c265fd2a
Piorozla <piorozla@o2.pl>
2017-10-29 21:12:15 +0000

    Heroku troubleshooting

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
-const app = express();
+let app = express();
 
 app.use(express.static(publicPath));
 
