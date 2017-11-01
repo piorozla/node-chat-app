82ad64427d409c5879fa1a432d4a52365195dd04
Piorozla <piorozla@o2.pl>
2017-11-01 21:11:40 +0000

    Greet new user and aler others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on("connect", (socket) => {
   console.log("New user connected");
 
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime(),
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New user joined the chat app",
+    createdAt: new Date().getTime(),
+  });
+
   socket.on("createMessage", (message) => {
     console.log(`New message to ${message.from}: ${message.text}`);
     io.emit("newMessage", {
@@ -21,6 +33,11 @@ io.on("connect", (socket) => {
       text: message.text,
       createdAt: new Date().getTime(),
     });
+    // socket.broadcast.emit("newMessage", {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime(),
+    // });
   });
 
   socket.on("disconnect", () => {
