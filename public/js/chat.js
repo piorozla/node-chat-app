ec97db2b294a19d9340092414e41225445a60d24
Piorozla <piorozla@o2.pl>
2017-11-10 19:00:15 +0000

    Added sidebar animation and button

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,19 +1,12 @@
 const socket = io();
+let sidebarToggle = "open";
 
-function scrollToBottom() {
-  const messages = $("#messages");
-  const newMessage = messages.children("li:last-child");
+const sliderButton = $("#slider");
+const sidebar = $(".chat__sidebar");
+const locationButton = $("#send-location");
 
-  const clientHeight = messages.prop("clientHeight");
-  const scrollTop = messages.prop("scrollTop");
-  const scrollHeight = messages.prop("scrollHeight");
-  const newMessageHeight = newMessage.innerHeight();
-  const lastMessageHeight = newMessage.prev().innerHeight();
-
-  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
-    messages.scrollTop(scrollHeight);
-  }
-}
+$(window).resize(checkWindowSize);
+checkWindowSize(); // checks windows size to see if the sidebar should be open
 
 socket.on("connect", () => {
   const params = $.deparam(window.location.search);
@@ -79,8 +72,6 @@ $("#message-form").on("submit", (e) => {
   });
 });
 
-const locationButton = $("#send-location");
-
 locationButton.on("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation not supported by your browser");
@@ -99,3 +90,56 @@ locationButton.on("click", () => {
   });
 });
 
+// open/close the sidebar on slider click
+sliderButton.on("click", () => {
+  if (sidebarToggle === "open") {
+    toggleSidebar("close");
+  } else {
+    toggleSidebar("open");
+  }
+});
+
+sliderButton.hover(() => { sliderButton.css("opacity", "1"); }, () => { sliderButton.css("opacity", "0.4"); });
+
+function scrollToBottom() {
+  const messages = $("#messages");
+  const newMessage = messages.children("li:last-child");
+
+  const clientHeight = messages.prop("clientHeight");
+  const scrollTop = messages.prop("scrollTop");
+  const scrollHeight = messages.prop("scrollHeight");
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
+function checkWindowSize() {
+  if (window.innerWidth < 600 && sidebarToggle === "open") {
+    toggleSidebar("close");
+  } else if (window.innerWidth >= 600 && sidebarToggle === "closed") {
+    toggleSidebar("open");
+  }
+}
+
+function toggleSidebar(action) {
+  if (action === "close") {
+    sidebar.animate({ "flex-basis": 0 }, 200);
+    sliderButton.animate({ left: 0 }, 200, () => {
+      sliderButton.html("<span>></span>")
+        .mouseleave()
+        .css("padding", "0.2rem 0.75rem 0.3rem 1rem");
+    });
+    sidebarToggle = "closed";
+  } else {
+    sidebar.animate({ "flex-basis": "260px" }, 200);
+    sliderButton.animate({ left: "15.5rem" }, 200, () => {
+      sliderButton.html("<span><</span>")
+        .mouseleave()
+        .css("padding", "0.2rem 0.75rem 0.3rem 0.75rem");
+    });
+    sidebarToggle = "open";
+  }
+}
